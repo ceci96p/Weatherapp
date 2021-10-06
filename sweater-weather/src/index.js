d909636b025b0bb6bd75def9a980b6093da1e6a2
Cecilia Mesquita Pacheco <cecilia.mesquita.pacheco@gmail.com>
2021-10-06 01:31:02 -0400

    cleanup master code

--- sweater-weather/src/index.js
+++ sweater-weather/src/index.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './index.css';
-
-// import App from './App';
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App/>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -27,25 +12,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import './App.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-// //import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-// //registerServiceWorker();
-
