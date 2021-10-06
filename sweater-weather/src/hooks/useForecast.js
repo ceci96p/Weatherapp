f4fe983a5920259c78e887c8d440b239767ea57c
Cecilia Mesquita Pacheco <cecilia.mesquita.pacheco@gmail.com>
2021-10-06 01:25:43 -0400

    cleanup code

--- sweater-weather/src/hooks/useForecast.js
+++ sweater-weather/src/hooks/useForecast.js
@@ -6,10 +6,6 @@ import getCurrentDayDetailedForecast from "../helpers/getCurrentDayDetailedForec
 import getUpcomingDaysForecast from "../helpers/getUpcomingDaysForecast";
 
 
-// const BASE_URL = 'https://www.metaweather.com/api/location';
-// const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com';
-// const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
-
 const BASE_URL = 'https://www.metaweather.com/api/location';
 const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com';
 const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
